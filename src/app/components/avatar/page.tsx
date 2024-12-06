a9b713c0a6bd4ab7b4e673c980bc439ebd0d2651
Kunal <kunalpaul673@gmail.com>
2024-12-06 21:16:14 +0530

    Latest Commit

--- src/app/components/avatar/page.tsx
+++ src/app/components/avatar/page.tsx
@@ -7,15 +7,15 @@ import { S3Client, GetObjectCommand, PutObjectCommand,ListObjectsCommand } from 
 // function FetchBucketData(){
 
 
-const s3Client = new S3Client({
-    region: process.env.NEXT_PUBLIC_AWS_REGION,
-    credentials: {
-      accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY!,
-      secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_KEY!,
-    },
-  });
 
-export async function FetchExistingImage(username: string): Promise<string[] | null> {
+export default async function FetchExistingImage(username: string): Promise<string[] | null> {
+  const s3Client = new S3Client({
+      region: process.env.NEXT_PUBLIC_AWS_REGION,
+      credentials: {
+        accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY!,
+        secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_KEY!,
+      },
+    });
   const params = {
       Bucket: process.env.NEXT_PUBLIC_BUCKET, // Replace with your S3 bucket name
       Prefix: `${username}/`, // Prefix the search with the username
@@ -39,51 +39,3 @@ export async function FetchExistingImage(username: string): Promise<string[] | n
 
 
 
-//   try {
-//     const params = {
-//       Bucket: process.env.NEXT_PUBLIC_BUCKET,
-//       Prefix: username,
-//     }
-
-//     const data = await s3Client.send(new GetObjectCommand(params))
-
-//     if (data.Contents && data.Contents.length > 0) {
-//       const matchingObject = data.Contents.find(obj => obj.Key?.includes(username))
-      
-//       if (matchingObject && matchingObject.Key) {
-//         const getObjectParams = {
-//           Bucket: "your-bucket-name",
-//           Key: matchingObject.Key,
-//         }
-
-//         const command = new GetObjectCommand(getObjectParams)
-//         const signedUrl = await getSignedUrl(s3Client, command, { expiresIn: 3600 })
-        
-//         return signedUrl
-//       }
-//     }
-
-//     return null
-//   } catch (error) {
-//     console.error("Error fetching existing image:", error)
-//     return null
-//   }
-// }
-// async function updateImage(username: string, newImageFile: File): Promise<boolean> {
-//   try {
-//     const fileContent = await newImageFile.arrayBuffer()
-//     const params = {
-//       Bucket: "your-bucket-name",
-//       Key: `${username}-${Date.now()}.${newImageFile.name.split('.').pop()}`,
-//       Body: Buffer.from(fileContent),
-//       ContentType: newImageFile.type,
-//     }
-
-//     await s3Client.send(new PutObjectCommand(params))
-//     return true
-//   } catch (error) {
-//     console.error("Error updating image:", error)
-//     return false
-//   }
-// }
-// }
