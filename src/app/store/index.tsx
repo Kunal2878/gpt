2be1fb666bccd2fc5923551b46fdef9351f1c17f
Kunal <kunalpaul673@gmail.com>
2024-12-29 10:34:21 +0530

    Remove commented-out signUpVerificationMethod in authConfig

--- src/app/store/index.tsx
+++ src/app/store/index.tsx
@@ -6,7 +6,7 @@ export const authConfig: ResourcesConfig["Auth"] = {
   Cognito: {
     userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID ?? '',
     userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_APP_CLIENT_ID ?? '',
-    // signUpVerificationMethod: 'code'
+ 
   }
 };
 
