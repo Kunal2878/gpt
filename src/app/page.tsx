53b3a3faf425b5c4b9029466b250e90dfdb40e55
Kunal <kunalpaul673@gmail.com>
2025-01-02 13:23:18 +0530

    Update main background style in Home component; replace commented-out code with gradient background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,7 @@ import { cookies } from 'next/headers'
 export default function Home() {
 const cookieStore = cookies()
 const cookieData = cookieStore.get("CognitoIdentityServiceProvider.58naai034kd5bn6itu7cip557h.LastAuthUser")
- 
-{/* <main className=" bg-white/70 flex min-h-screen flex-col "> */}
+
 return (
  <main className=" bg-gradient-to-r from-black  to-indigo-700 flex min-h-screen flex-col "> 
 
